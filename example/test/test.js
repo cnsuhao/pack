2aa752a867cf3d73dc296228f5077549d7990cda
TerryWu <terry.wuhao@gmail.com>
2014-03-13 20:32:06 +0800

    Update test.js

--- example/test/test.js
+++ example/test/test.js
@@ -1,5 +1,5 @@
 define(function(require,exports,module) {
-	var tpl = require("/test/test.html"); // 加载资源模板
+	var tpl = require("/test/template.html"); // 加载资源模板
 	alert(tpl);
 	exports.start = function() {
 		alert('start');
