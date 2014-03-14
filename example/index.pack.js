f85f482495b44894f2893162672793ed4d7c73e6
TerryWu <terry.wuhao@gmail.com>
2014-03-14 10:12:41 +0800

    Update index.pack.js

--- example/index.pack.js
+++ example/index.pack.js
@@ -1,22 +1,22 @@
-//PACK ROOT=D:\data\htdocs\pack\example
+//PACK ROOT=D:\data\htdocs\pack
 (function() {
 
 	var modules = {};
-modules["/test/test.html"] = "<div>\"TEST-TEMPLATE\"</div>";
-modules["/test/test.js"] = {exports: {}};
+modules["/example/test/test.html"] = "<div>\"TEST-TEMPLATE\"</div>";
+modules["/example/test/test.js"] = {exports: {}, uri: "/example/test/test.js"};
 (function (require,exports,module) {
-	var tpl = require("/test/test.html"); // 加载资源模板
+	var tpl = require("/example/test/test.html"); // 加载资源模板
 	alert(tpl);
 	exports.start = function() {
 		alert('start');
 	};
-})(_require, modules["/test/test.js"].exports, modules["/test/test.js"]);
-modules["/index.js"] = {exports: {}};
+})(_require, modules["/example/test/test.js"].exports, modules["/example/test/test.js"]);
+modules["/example/index.js"] = {exports: {}, uri: "/example/index.js"};
 (function (require,exports,module) {
-	var mongo = require("/test/test.js"); // 加载模块
+	var mongo = require("/example/test/test.js"); // 加载模块
 	mongo.start();
-})(_require, modules["/index.js"].exports, modules["/index.js"]);
+})(_require, modules["/example/index.js"].exports, modules["/example/index.js"]);
 function _require(id) { if(typeof(modules[id]) === "string"){ return modules[id]; } else { return modules[id].exports; } }
-_require("/index.js");
+_require("/example/index.js");
 
 })();
