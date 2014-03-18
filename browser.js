882478046d147d35eef379539f1e2eb6f75f85ff
TerryWu <terry.wuhao@gmail.com>
2014-03-18 14:20:08 +0800

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,40 +1,30 @@
 (function() {
-	// 实际加载资源的 require 函数
-	function _require(path) {
-		var x = new XMLHttpRequest();
-		x.open("GET",path,false);
-		var $$_exports;
+	var $$_exports, $$_head = document.getElementsByTagName('HEAD')[0];
+
+	function _require2(path) {
 		if(path.match(/\.js$/)) {
-			x.onreadystatechange = function() {
-				if(x.readyState == 4) {
-					if(path[0] === '/') {
-						path = location.origin+path;
-					}else{
-						var p1 = location.href.split('/'), p2 = path.split('/');
-						p1.pop(); p2.shift();
-						while(p2[0] === '..') {
-							p2.shift();
-							p1.pop();
-						}
-						path = p1.join('/')+'/'+p2.join('/');
-					}
-					eval('$$_exports = '+x.responseText+'\n//# sourceURL='+path);
-				}
-			}
+			var script = document.createElement('script');
+			// script.onload = function() {};
+			script.src   = path;
+			script.async = false;
+			$$_head.appendChild(script);
 		}else{
-			x.onreadystatechange = function() {
-				if(x.readyState == 4) {
-					$$_exports = x.responseText;
-				}
-			}
+			var x = new XMLHttpRequest();
+			x.open("GET",path,false); // 同步加载
+			x.onreadystatechange = function() { if(x.readyState == 4) $$_exports = x.responseText; }
+			x.send();
 		}
-		x.send();
-		return $_exports;
+		return $$_exports;
 	}
+	
 	// 模块定义函数
 	window.define = function(def) {
 		var module = {exports: {}};
-		def(_require, module.exports, module);
-		return module.exports;
+		var exports = def(_require2, module.exports, module);
+		if(exp) {
+			$$_exports = exports;
+		}else{
+			$$_exports = module.exports;
+		}
 	};
 })();
