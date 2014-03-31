d868ee9a3ba16c8a88ca701c1089e463ee9d7d0c
TerryWu <terry.wuhao@gmail.com>
2014-03-31 16:36:29 +0700

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,30 +1,82 @@
 (function() {
-	var $$_exports, $$_head = document.getElementsByTagName('HEAD')[0];
+	var _defined = {}, _depended = {}, UID = 0,
+		_head = document.getElementsByTagName('HEAD')[0];
 
-	function _require2(path) {
-		if(path.match(/\.js$/)) {
-			var script = document.createElement('script');
-			// script.onload = function() {};
-			script.src   = path;
-			script.async = false;
-			$$_head.appendChild(script);
-		}else{
-			var x = new XMLHttpRequest();
-			x.open("GET",path,false); // 同步加载
-			x.onreadystatechange = function() { if(x.readyState == 4) $$_exports = x.responseText; }
-			x.send();
+	var extract_require = function(str, deps) {
+		var p = /require\s*\(\s*["']([^"']+)["']\s*\)/g,
+			t;
+
+		while(p.exec(str)) {
+			t = RegExp.$1;
+			if(t.match(/\.js$/)) {
+				deps.push(t);
+			}else{
+				var x = new XMLHttpRequest();
+				x.open('GET',t,false);
+				x.onreadystatechange = function() {
+					if(x.readyState == 4) {
+						_defined[ t ] = {exports: x.responseText};
+					}
+				};
+				x.send();
+			}
 		}
-		return $$_exports;
+	};
+
+	var notify_depended = function(path) {
+		if(_depended[path]) {
+			_depended[path].forEach(function(item) {
+				-- _defined[item].depends;
+				if(_defined[item].depends === 0) {
+					actual_define(item);
+				}
+			});
+		}
+	};
+
+	var actual_define = function(path) {
+		var _export = _defined[path]['define'](function(p) {
+				return _defined[p]['exports'];
+			}, _defined[path]['exports'], _defined[path]);
+		if(_export) {
+			_defined[path].exports = _export;
+		}
+		// 通知依赖的项
+		notify_depended(path);
 	}
-	
+
+
 	// 模块定义函数
 	window.define = function(def) {
-		var module = {exports: {}};
-		var exports = def(_require2, module.exports, module);
-		if(exp) {
-			$$_exports = exports;
+		++UID;
+
+		var path = document.currentScript.src.substr(location.origin.length),
+			deps = [];
+
+		
+		// 提取依赖项
+		extract_require(def.toString(), deps);
+		// 模块定义
+		_defined[path] = { exports: {}, define: def, path: path, depends: deps.length };
+
+		if(deps.length > 0) {
+			// 依赖关系整理
+			for(var i=0;i<deps.length;++i) {
+				if(_depended[deps[i]]) {
+					_depended[deps[i]].push(path);
+				}else{
+					_depended[deps[i]] = [path];
+				}
+			}
+
+			while(deps.length>0) {
+				var depend = deps.pop();
+				if(!_defined[depend]) {
+					document.write( '<script async="false" defer="false" src="'+depend+'"></script>' );
+				}
+			}
 		}else{
-			$$_exports = module.exports;
+			actual_define(path);
 		}
 	};
 })();
